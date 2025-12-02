bd660b7d10d00433e9206eb4e09aaae802dfc2b6
Carlos Salas <carlos.salas@suse.com>
2025-12-02 20:33:48 +0100

    add day 01, part 2
    Signed-off-by: Carlos Salas <carlos.salas@suse.com>

--- aoc_2025/src/bin/day01.rs
+++ aoc_2025/src/bin/day01.rs
@@ -3,9 +3,14 @@ use std::fs;
 fn main() {
     //let file_path = "./inputs/day01_test";
     let file_path = "./inputs/day01";
-    println!("Read input from file {file_path}");
 
-    let mut start: i32 = 50;
+    part2(file_path);
+}
+
+pub fn part1(file_path: &str) {
+    println!("Part 1");
+
+    let mut pos: i32 = 50;
     let mut zeroes: i32 = 0;
     // make it so it circles from 0-99
     // if 50 - 60, it will go back to 90
@@ -13,24 +18,68 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     for line in contents.lines() {
-        let (letter_part, number_part) = line.split_at(1);
-        let letter = letter_part.chars().next().unwrap();
+        let (letter, number_part) = line.split_at(1);
         let number: i32 = number_part.parse().unwrap();
 
-        let value = match letter {
-            'L' => -1,
-            'R' => 1,
-            _ => panic!("Unexpected letter"),
-        };
+        match letter {
+            "R" => {
+                pos = (pos + number).rem_euclid(100);
+            }
+            "L" => {
+                pos = (pos - number).rem_euclid(100);
+            }
+            _ => panic!("Invalid operation"),
+        }
 
-        let rotation: i32 = number * value;
-
-        let result = (start + rotation).rem_euclid(100);
-        if result == 0 {
+        if pos == 0 {
             zeroes += 1;
         }
-        start = result
     }
 
-    println!("Password: {}", zeroes);
+    println!("Password part one: {}", zeroes);
+}
+
+pub fn part2(file_path: &str) {
+    println!("Part 2");
+
+    let mut absolute_pos: i32 = 50;
+    let mut zeroes_part_two: i32 = 0;
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for line in contents.lines() {
+        let (letter, number_part) = line.split_at(1);
+        let number: i32 = number_part.parse().unwrap();
+
+        zeroes_part_two += number.div_euclid(100);
+
+        let remainder = number % 100;
+
+        if remainder > 0 {
+            let dist_to_zero = match letter {
+                "R" => 100 - absolute_pos,
+                "L" => {
+                    if absolute_pos == 0 {
+                        100
+                    } else {
+                        absolute_pos
+                    }
+                }
+                _ => panic!("Invalid operation"),
+            };
+            if remainder >= dist_to_zero {
+                zeroes_part_two += 1;
+            }
+        }
+        match letter {
+            "R" => {
+                absolute_pos = (absolute_pos + number).rem_euclid(100);
+            }
+            "L" => {
+                absolute_pos = (absolute_pos - number).rem_euclid(100);
+            }
+            _ => panic!("Invalid operation"),
+        }
+    }
+
+    println!("Password part two: {}", zeroes_part_two);
 }
